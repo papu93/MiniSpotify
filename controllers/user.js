6417e68572967dcb46f305d22af5f8eef74f0067
Damian Dominguez <damian93domin@gmail.com>
2017-05-20 16:41:36 -0300

    Login usuario

--- controllers/user.js
+++ controllers/user.js
@@ -48,8 +48,40 @@ function saveUser(req, res){
 	}
 }
 
+function loginUser(req,res){
+	var params = req.body;
+
+	var email = params.email;
+	var password = params.password;
+
+	User.findOne({email: email.toLowerCase()}, (err,user) => {
+		if(err){
+			res.status(500).send({message: 'Error en la peticion'});
+		}else{
+			if(!user){
+				res.status(404).send({message: 'El usuario no existe'});
+			}else{
+				//comprobar la contrasena
+				bcrypt.compare(password,user.password, function(err,check){
+					if(check){
+						//devolver datos de usuario logueado
+						if(params.gethash){
+							//devolver un token de jwt
+						}else{
+							res.status(200).send({user});
+						}
+					}else{
+						res.status(404).send({message: 'La contrasena es incorrecta'});
+					}
+				});
+			}
+		}
+	})
+}
+
 module.exports = {
 	pruebas,
-	saveUser
+	saveUser,
+	loginUser
 	//mas metodos
 };
--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,6 @@
 
  api.get('/probando-controlador',UserController.pruebas);
  api.post('/register',UserController.saveUser);
+ api.post('/login',UserController.loginUser);
 
  module.exports = api;
