9367e225b9c52dfaeda7dca641b143ec5f5ef0e4
Damian Dominguez <damian93domin@gmail.com>
2017-05-20 00:25:10 -0300

    Guardar usuario

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,55 @@
 'use strict'
 
+var bcrypt = require('bcrypt-nodejs');
+var User = require('../models/users');
+
 function pruebas(req, res){
 	res.status(200).send({
 		message: 'Probando una accion del controlador de usuarios del api rest con Node y Mongo'
 	});	
 }
 
+function saveUser(req, res){
+	var user = new User();
+
+	var params = req.body; //Param que lleguen por POST
+
+	console.log(params);
+
+	user.name = params.name;
+	user.surname = params.surname;
+	user.email = params.email;
+	user.role = 'ROLE_ADMIN';
+	user.image = 'null';
+
+	if(params.password){
+		//Encriptar contrasena y guardar dato
+		bcrypt.hash(params.password, null, null, function(err,hash){
+			user.password = hash;
+			if(user.name != null && user.surname != null && user.email != null){
+				//Guardar usuario
+				user.save((err, userStored) => {
+					if(err){
+						res.status(500).send({message: 'Error al guardar el usuario'});
+					}else{
+						if(!userStored){
+							res.status(400).send({message: 'No se ha registrado el usuario'});
+						}else{
+							res.status(200).send({user:userStored});
+						}
+					}
+				}); 
+			}else{
+				res.status(200).send({message: 'Rellena todos los campos'});
+			}
+		});
+	}else{
+		res.status(500).send({message: 'Introduce la contrasena'});
+	}
+}
+
 module.exports = {
-	pruebas
+	pruebas,
+	saveUser
 	//mas metodos
 };
