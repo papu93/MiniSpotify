fccb1b4fbb613156d8d01575b08398a956ca79e7
Damian Dominguez <damian93domin@gmail.com>
2017-05-27 18:40:29 -0300

    Cargar y pedir una cancion

--- controllers/song.js
+++ controllers/song.js
@@ -108,10 +108,59 @@ function deleteSong(req,res){
 	});
 }
 
+function uploadFile(req,res){
+//Metodo para subir ficheros
+	var songId = req.params.id; 
+	var file_name = 'Cancion no subida'; //por defecto
+
+	if(req.files){
+		var file_path = req.files.file.path;
+		var file_split = file_path.split('/'); //recortar la direc de la img
+		var file_name = file_split[2]; //elegimos solo el nombre
+
+		var ext_split = file_name.split('.'); //separo el nombre de la extension
+		var file_ext = ext_split[1]; //me quedo con la extension
+
+		if(file_ext == 'mp3' || file_ext == 'ogg'){
+			Song.findByIdAndUpdate(songId, {file: file_name},(err,songUpdated) => {
+				if(err){
+					res.status(500).send({message:'Error al actuaizar la cancion'});
+				}else{
+					if(!songUpdated){
+						res.status(404).send({message:'No se ha podido actualizar la cancion'});
+					}else{
+						res.status(200).send({song: songUpdated});
+					}
+				}
+			});
+		}else{
+			res.status(200).send({message: 'Extension del archivo invalida'});
+		}
+	}else{
+		res.status(200).send({message: 'No has subido ninguna cancion'});
+	}
+}
+
+function getSongFile(req, res){
+	var songFile = req.params.songFile; //obtengo nombre de la imagen solicitada
+	var path_file = './uploads/songs/'+songFile; //agrego la ruta
+	
+	fs.exists(path_file,function(exists){ 
+		if(exists){ 
+			res.sendFile(path.resolve(path_file));
+		}else{
+			res.status(200).send({message: 'No existe la cancion'});
+		}
+	});
+}
+
+
 module.exports = {
 	getSong,
 	saveSong,
 	getSongs,
 	updateSong,
-	deleteSong
+	deleteSong,
+	uploadFile,
+	getSongFile
 }
