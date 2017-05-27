64fa4f08f956a23ea989083a08e120297b169681
Damian Dominguez <damian93domin@gmail.com>
2017-05-27 15:02:12 -0300

    Album agregado con su get y save

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var app = express();
 //cargar RUTAS
 var user_routes = require('./routes/user');
 var artist_routes = require('./routes/artist');
+var album_routes = require('./routes/album');
 
 app.use(bodyParser.urlencoded({extended:false})); //Necesario para q bodyParser ande
 app.use(bodyParser.json()); //convierto a obj JSON lo que viene en las peticiones
@@ -17,5 +18,6 @@ app.use(bodyParser.json()); //convierto a obj JSON lo que viene en las peticione
 //rutas base
 app.use('/api',user_routes);
 app.use('/api',artist_routes);
+app.use('/api',album_routes)
 
 module.exports = app;
---
+++ controllers/album.js
@@ -0,0 +1,41 @@
+'use strict'
+
+var path = require('path');
+var fs = require('fs');
+var mongoosePaginate = require('mongoose-pagination');
+
+var Artist = require('../models/artist');
+var Album = require('../models/album');
+var Song = require('../models/song');
+
+function getAlbum(req,res){ 
+	res.status(200).send({message: 'Accion getAlbum'});
+}
+
+function saveAlbum(req,res){
+	var album = new Album();
+
+	var params = req.body;
+	album.title = params.title;
+	album.description = params.description;
+	album.year = params.year;
+	album.image = 'null';
+	album.artist = params.artist;
+
+	album.save(function(err,albumStored){
+		if(err){
+			res.status(500).send({message: 'Error al guardar el album'});
+		}else{
+			if(!albumStored){
+				res.status(404).send({message: 'No se ha guardado el album'});
+			}else{
+				res.status(200).send({album: albumStored});
+			}
+		}
+	});
+}
+
+module.exports = {
+	getAlbum,
+	saveAlbum
+}
