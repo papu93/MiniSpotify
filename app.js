524d746281e53a347fd3fd38382f5b4dd0fdd624
Damian Dominguez <damian93domin@gmail.com>
2017-05-28 00:14:43 -0300

    Configuracion cabeceras HTTP

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ app.use(bodyParser.urlencoded({extended:false})); //Necesario para q bodyParser 
 app.use(bodyParser.json()); //convierto a obj JSON lo que viene en las peticiones
 
 //configurar cabeceras HTTP
+app.use(function(req,res,next){ //Configuracion para que ande AJAX
+	res.header('Access-Control-Allow-Origin','*');
+	res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Method', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow','GET, POST, OPTIONS, PUT, DELETE');
+
+	next();
+});
 
 //rutas base
 app.use('/api',user_routes);
