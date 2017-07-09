586e80ad0ddc27e956921dedf950445de1547684
Damian Dominguez <damian93domin@gmail.com>
2017-07-09 00:58:06 -0300

    Cargar imagen usuario

--- client/src/app/components/user-edit.component.ts
+++ client/src/app/components/user-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { User } from '../models/user';
+import { GLOBAL } from '../services/global';
+
 
 @Component({
 	selector: 'user-edit',
@@ -14,6 +16,7 @@ export class UserEditComponent implements OnInit{
 	public identity;
 	public token;
 	public alertMessage;
+	public url: string;
 
 	constructor(
 		private _userService: UserService
@@ -22,6 +25,7 @@ export class UserEditComponent implements OnInit{
 		this.identity = this._userService.getIdentity();
 		this.token = this._userService.getToken();
 		this.user = this.identity;
+		this.url = GLOBAL.url;
 	}
 
 	ngOnInit(){
@@ -37,6 +41,20 @@ export class UserEditComponent implements OnInit{
 					//this.user = response.user; //actualizamos usuario
 					localStorage.setItem('identity', JSON.stringify(this.user)); //actualizamos lo guardado
 					document.getElementById("identity_name").innerHTML = this.user.name;
+
+					if(!this.filesToUpload) { //Si no existe archivos no sube nada
+						//redireccion
+						console.log("NO HAY FILES TO UPLOAD");
+					} else {
+
+						this.makeFileRequest(this.url + 'upload-image-user/' + this.user._id, [], this.filesToUpload).then(
+							(result: any) => {
+								this.user.image = result.image; //agregamos la img y recargamos el localStorege
+								localStorage.setItem('identity', JSON.stringify(this.user)); 
+								console.log(this.user);
+							}
+						);
+					}
 					this.alertMessage = 'Datos actualizados correctamente';
 				}
 			}, 
@@ -51,4 +69,41 @@ export class UserEditComponent implements OnInit{
 				}
 			});
 	}
+
+	public filesToUpload: Array<File>;
+
+	fileChangeEvent(fileInput:any) {
+		this.filesToUpload = <Array<File>>fileInput.target.files; //recoge los arch seleccionados en el input
+	}
+
+	makeFileRequest(url: string, params: Array<string>, files:Array<File>) {
+
+		var token = this.token; 
+		return new Promise(function(resolve, reject) {
+
+			var formData: any = new FormData(); //Simular comportamiento de un formulario
+			var xhr = new XMLHttpRequest(); 
+
+			for (var i = 0; i < files.length; i++) {
+				formData.append('image', files[i], files[i].name);
+			}
+
+			xhr.onreadystatechange = function() { //si esta lista la peticion para realizarse
+				if(xhr.readyState == 4) {
+					if(xhr.status == 200) {
+						resolve(JSON.parse(xhr.response)); //respuesta de enviar el archivo
+					} else {
+						reject(xhr.response);
+					}
+				}
+			}
+
+			xhr.open('POST', url, true);
+			xhr.setRequestHeader('Authorization', token);
+			xhr.send(formData);
+		});
+	}
 }
+
+
+
