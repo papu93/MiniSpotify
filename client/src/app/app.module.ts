2a65dd7b6d46fe2e824019559856032874db4457
Damian Dominguez <damian93domin@gmail.com>
2017-07-09 16:46:29 -0300

    Componente artista

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { UserEditComponent } from './components/user-edit.component';
+import { ArtistListComponent } from './components/artist-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserEditComponent
+    UserEditComponent,
+    ArtistListComponent
   ],
   imports: [
     BrowserModule,
